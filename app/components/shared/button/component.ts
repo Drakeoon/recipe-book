1a9d0d2f4de0cff4da6cc4ca9d91da01326c3362
Daniel Szczepanik <tmlm18@gmail.com>
2020-06-20 15:56:48 +0200

    Initial config, components and layout

---
+++ app/components/shared/button/component.ts
@@ -0,0 +1,9 @@
+import Component from '@glimmer/component';
+
+interface SharedButtonArgs {
+  onClick(): void;
+}
+
+export default class SharedButton extends Component<SharedButtonArgs> {
+
+}
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -2,9 +2,15 @@
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
-module.exports = function(defaults) {
+module.exports = function (defaults) {
   let app = new EmberApp(defaults, {
     // Add options here
+    sassOptions: {
+      extension: "sass",
+      includePaths: [
+        'node_modules/tailwindcss'
+      ]
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
